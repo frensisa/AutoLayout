7234a9b35bf66827bea08f04e39be6ee7b6946d9
frensisa <112398338+frensisa@users.noreply.github.com>
2023-04-14 15:45:24 +0300

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by frensisa daudze on 14/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
