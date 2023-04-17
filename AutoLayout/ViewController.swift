867e47a03d5674b562580b3455b7d24c9da5189e
frensisa <112398338+frensisa@users.noreply.github.com>
2023-04-17 15:19:33 +0300

    hmsubmit

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var butE: UIButton!
+    @IBOutlet weak var butA: UIButton!
+    @IBOutlet weak var butB: UIButton!
+    @IBOutlet weak var butD: UIButton!
+    @IBOutlet weak var butC: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        butA.layer.cornerRadius = 8
+        butB.layer.cornerRadius = 8
+        butC.layer.cornerRadius = 8
+        butD.layer.cornerRadius = 8
+        butE.layer.cornerRadius = 8
     }
 
 
